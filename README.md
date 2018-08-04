fc51034069688ff3b6669b1e9a01ab43a4e29f3d
harshithajnaik <naikharshitha77@gmail.com>
2018-08-04 20:19:02 +0530

    updated README.md file and chat_bot.py

--- README.md
+++ README.md
@@ -1,17 +1,22 @@
 # CoffeeBot
 
-A coffee ordering bot that takes user coffee orders and returns the price.
+A coffee ordering bot that takes user coffee orders and returns the price based on the number of cups of coffes as well as the size of the cups i.e Regular, Medium or Large.
+
 
 ## Getting Started
 
 These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
 
+You will be needing a Telegram account for creating a new bot. The machine learning component ,IBM Watson let's you to extract the intents and entities.
 ### Installing
 
-* Install lastest version of Python 2 from [Python(Download link)](https://www.python.org/downloads/) by chosing the appropriate link for Windows/Mac/Linux,Other.
+* Install lastest version of Python 3 from [Python(Download link)](https://www.python.org/downloads/) by chosing the appropriate link for Windows/Mac/Linux,Other.
 
 * Install GitHub from [Git (Download link)](https://git-scm.com/downloads).
 
+*Install Telegram app from[Telegram(Downlaod link)](https://telegram.org).
+
+*Creating IBM Watson account from[IBM Watson(Download link)](https://console.bluemix.net/registration/?target=%2Fdeveloper%2Fwatson%2Fservices).
 ## Deployment
 
 After successful installation of GitHub, open git bash in the folder where you want this project to be cloned.
--- chat_bot.py
+++ chat_bot.py
@@ -4,34 +4,22 @@ import json
 import sqlite3
 from sqlite3 import Error
  
-
- 
-"""
-        cur.execute("CREATE TABLE IF NOT EXISTS user_ord(type text,size text,number real)")
-        cur.execute("CREATE TABLE IF NOT EXISTS coffee_types(type text,price real)")
-        cofee = [('black coffee', '25'),
-             ('irish coffee', '25'),
-             ('cappuccino', '25'),('macchiato', '25'),('espresso', '25'),('café latte', '25'),('turkish coffee', '25'),('frappuccino', '25'),('iced coffee', '25'),('flat white', '25'),
-            ]
-        cur.executemany('INSERT INTO coffee_types VALUES(?,?)',cofee) 
+#to create a table initially. Uncomment if running the code first time.
+'''conn = sqlite3.connect("coffee_order.db")
+cur = conn.cursor()
+print(conn ,cur)
+cur.execute("CREATE TABLE IF NOT EXISTS user_ord(type text,size text,number real)")
+cur.execute("CREATE TABLE IF NOT EXISTS coffee_types(type text,price real)")
+cofee = [('black coffee', '35'),('irish coffee', '45'),('cappuccino', '25'),('macchiato', '40'),('espresso', '25'),('cafe latte', '50'),('turkish coffee', '25'),('frappuccino', '60'),('iced coffee', '45'),('flat white', '35'),]
+cur.executemany('INSERT OR IGNORE INTO coffee_types VALUES(?,?)',cofee) 
        
-        #cur.execute("INSERT INTO user_ord VALUES ('black coffee','medium',2)")
-        conn.commit()
-        cur.execute("SELECT * FROM coffee_types")
-        #print cur.fetchone()
-        rows = cur.fetchall()
-
-        for row in rows:
-            print(row)	
-     
-        cur.execute("CREATE TABLE coffee")		
-       cofee = [('black coffee', '25'),
-             ('irish coffee', '25'),
-             ('cappuccino', '25'),('macchiato', '25'),('espresso', '25'),('café latte', '25'),('turkish coffee', '25'),('frappuccino', '25'),('iced coffee', '25'),('flat white', '25'),
-            ]
-        cur.executemany('INSERT INTO coffee VALUES(?,?)',cofee) 
-       
-"""
+    #cur.execute("INSERT INTO user_ord VALUES ('black coffee','medium',2)")
+conn.commit()
+cur.execute("SELECT * FROM coffee_types")
+    #print cur.fetchone()
+rows = cur.fetchall()
+for row in rows:
+    print(row)'''	
      
 
 context = None
@@ -44,6 +32,7 @@ finalord=[]
 currord=[]
 thisord=[]
 oldord=[]
+#send watson generated reponse to telegram
 def send_tele(response):
 
     resp = ''
@@ -51,76 +40,83 @@ def send_tele(response):
         resp += text
     print(resp)
     return resp
- 
+#Format python generated response and send to telegram
+def list_to_string(val):
+    resp = ''
+    for text in val:
+        resp += text+' '
+    print(resp)
+    return resp
+
+#generates response based on the missing value either quantity or size
 def handle_missing(bot,update):
     global context
     if len(thisord)==0:
         context={'missing':'null','types':'null','size':'null','anything':'yes'}
         return "Would you like to continue ordering? yes or no"
     for ord in thisord:
-        print('missing',ord[0])
         if 'number' not in ord[0]:
-            print("number isnt here")
             context={'missing':'yes','types':ord[0]['types'],'size':ord[0]['size']}
             val1=ord[0]['types']
             val2=ord[0]['size']
             del thisord[0]
-            print("after deletion",thisord)
-            return 'How many',val1,val2,'do you need?'
+            val='How many',val1,val2,'do you need?'
+            text_resp=list_to_string(val)
+            return text_resp
         if 'size' not in ord[0]:
-            print("size isnt here")
             context={'missing':'yes','types':ord[0]['types'],'number':ord[0]['number']}
             val1=ord[0]['types']
-            print(context)
             del thisord[0]
-            return 'Would you like to have',val1, 'it in regular,medium or large?'
+            val='Would you like to have',val1, 'it in regular,medium or large?'
+            text_resp=list_to_string(val)
+            return text_resp
+         
 
-
+#populates the list when input contains type and size or type and number and calls handle_missing() to generate response. 
 def handle_two(bot,update,order):
     global thisord
     thisord=[]
     for i in range(0,len(order),2):
         if order[i]['entity']=='types' and order[i+1]['entity']=='size':
-            print(order[i]['value'],':',order[i+1]['value'])
             currord=[{'types':order[i]['value'],'size':order[i+1]['value']}]
             thisord.append(currord)
           
         elif order[i]['entity']=='types' and order[i+1]['entity']=='number':
-            print(order[i]['value'],':',order[i+1]['value'])
             currord=[{'types':order[i]['value'],'number':order[i+1]['value']}]
             thisord.append(currord)
         elif order[i]['entity']=='size' and order[i+1]['entity']=='types':
-            print(order[i+1]['value'],':',order[i]['value'])
             currord=[{'size':order[i]['value'],'types':order[i+1]['value']}]
             thisord.append(currord)
-        elif order[i]['entity']=='number' and order[i+1]['entity']=='types': 
-            print(order[i+1]['value'],':',order[i]['value']) 
+        elif order[i]['entity']=='number' and order[i+1]['entity']=='types':  
             currord=[{'number':order[i]['value'],'types':order[i+1]['value']}]
             thisord.append(currord)
         else:
             print('Invalid') 
 
-    print('this ',thisord) 
+    print('Order recieved ',thisord) 
     val=handle_missing(bot,update)
     return val
 
+#generates response when size is missing and initial input contained only the type name.
 def handle_missing_one(bot,update):
     global context
     global oldord
     global thisord
-    print('latest',thisord)
     if len(thisord)==0:
         context={'missing':'null','types':'null','size':'null','anything':'yes'}
-        return "Would you like to continue ordering? yes or no"
+        return 'Would you like to continue ordering? yes or no'
     for ord in thisord:
         if 'size' not in ord[0]:
-            print("size isnt here")
             context={'missing':'yes','types':ord[0]['types'],'number':ord[0]['number'],'num':'null'}
             val1=ord[0]['types']
             del thisord[0]
             thisord=oldord.copy()
-            return 'Would you like to have',val1, 'it in regular,medium or large?'
+            val='Would you like to have',val1, 'it in regular,medium or large?'
+            text_resp=list_to_string(val)
+            print (text_resp)
+            return text_resp
 
+#parses the list order and calls handle_missing_one  
 def handle_size(bot,update,order):
     global thisord
     global oldord
@@ -128,28 +124,27 @@ def handle_size(bot,update,order):
     thisord=[]
     for i in range(0,len(order),1):
         if 'types' in order[i] and 'number' in order[i]:
-            print(order[i]['types'],':',order[i]['number'])
             currord=[{'types':order[i]['types'],'number':order[i]['number']}]
             thisord.append(currord)
-    print('this size',thisord) 
     val=handle_missing_one(bot,update)
     return val
 
+#parses the list from handle_one and requests quantity for each item 
 def handle_number():
     global context
     if len(thisord)==0:
         context={'missing':'null','types':'null','size':'null','anything':'yes'}
-        return "Would you like to continue ordering? yes or no"
+        return 'Would you like to continue ordering? yes or no'
     for ord in thisord:
         if 'number' not in ord[0]:
             context = {'missing': 'yes', 'types': ord[0]['types'], 'num':'null'}
-            print('aft context',context)
             val1 = ord[0]['types']
             del thisord[0]
-            print("after deletion", thisord)
-            return 'How many cups of', val1, 'do you need?'
+            val='How many cups of', val1, 'do you need?'
+            text_resp=list_to_string(val)
+            return text_resp
 
-
+#populates the list with the input-types and calls handle_number()
 def handle_one(order):
     global thisord
     thisord = []
@@ -160,6 +155,7 @@ def handle_one(order):
     val=handle_number()
     return val
 
+#populates the finalord list ,input contains all three values
 def handle_three(order):
     for i in range(0,len(order),3):
         if order[i]['entity']=='types' and order[i+1]['entity']=='size' and order[i+2]['entity']=='number':
@@ -182,41 +178,44 @@ def handle_three(order):
             finalord.append(currord)
         else:
             print('Invalid')
-        
+ #generates response for /start command       
 def start(bot, update):
     print('Received /start command')
-    update.message.reply_text('Hi!')
+    update.message.reply_text('Hi I am a coffee bot')
 
-
+#handles any text message sent through telegram 
 def message(bot, update):
     print('Received an update')
     global context 
     global final  
     global conversation 
-    conversation= ConversationV1(username='user name',  # TODO
-                                  password='*******',  # TODO
+    conversation= ConversationV1(username='username',  # TODO
+                                  password='password',  # TODO
                                   version='2018-02-16')
                                   
 
     # get response from watson
     response = conversation.message(
-        workspace_id='workspace ID',  # TODO
+        workspace_id='id',  # TODO
         input={'text': update.message.text},
         context=context)
-    #print(json.dumps(response, indent=2))
     context = response['context']
     order=response['entities']
-    #print(response)
     global finalord
+    global thisord
+    #when the recieved response is unknown. 
     if len(response['intents'])==0 and len(response['entities'])==0:
         resp2=send_tele(response)
         update.message.reply_text(resp2)
+    
+    #if any one the three values are missing, it enters this condition.    
     if 'missing' in context and context['missing']=='yes':
         if 'types' in context and 'size' in context:
             currord=[{'types':context['types'],'size':context['size'],'number':order[0]['value']}]
             finalord.append(currord)
             val=handle_missing(bot,update)
             update.message.reply_text(val)
+            
         if 'types' in context and 'number' in context and 'num' not in context:
             
             if len(order)==1 and order[0]['entity']=='size':
@@ -224,6 +223,7 @@ def message(bot, update):
                 finalord.append(currord)
                 val = handle_missing(bot, update)
                 update.message.reply_text(val)
+                
             else:
                 
                 for i in range(0,len(order),2):
@@ -238,10 +238,11 @@ def message(bot, update):
                         currord=[{'types':context['types'],'size':order[i+1]['value'],'number':order[i]['value']}]
                         
                         finalord.append(currord)
-                        print(finalord)
+                        print('Final Order',finalord)
                 val=handle_missing(bot,update)
                 update.message.reply_text(val)
-        print(finalord)
+                
+        print('Final Order',finalord)
         if 'types' in context and 'number' in context and 'num' in context and context['num']=='null':
             
             if len(order)==1 and order[0]['entity']=='size':
@@ -249,6 +250,7 @@ def message(bot, update):
                 finalord.append(currord)
                 val=handle_number()
                 update.message.reply_text(val)
+                
             else:
                 
                 for i in range(0,len(order),2):
@@ -263,15 +265,16 @@ def message(bot, update):
                         currord=[{'types':context['types'],'size':order[i+1]['value'],'number':order[i]['value']}]
                         
                         finalord.append(currord)
-                        print(finalord)
+                        print('Final Order',finalord)
                 val=handle_number()
                 update.message.reply_text(val)
-        print('final',finalord)
+                
+        print('Final Order',finalord)
         if 'types' in context  and 'num' in context and context['num']=='null' and order[0]['entity']=='number' and len(order)==1:
             currord=[{'types':context['types'],'number':order[0]['value']}]
-            print(currord)
             val = handle_size(bot,update,currord)
             update.message.reply_text(val)
+            
 
     elif len(order)==1 and order[0]['entity']=='types':
         val = handle_one(order)
@@ -284,6 +287,7 @@ def message(bot, update):
         if t==len(order):
             val = handle_one(order)
             update.message.reply_text(val)
+            
         elif len(order)%2 == 0 and len(order)%3==0:
             t=0
             for j in range(len(order)):
@@ -296,7 +300,9 @@ def message(bot, update):
                 context = {'anything': 'yes'}
             elif (len(order)/t)%2==0:
                 val=handle_two(bot,update,order)
+                print(thisord)
                 update.message.reply_text(val)
+                
         elif len(order)%3 ==0:
             handle_three(order)
             update.message.reply_text("Would you like to continue ordering? yes or no")
@@ -304,10 +310,8 @@ def message(bot, update):
         elif len(order)%2==0:
             val=handle_two(bot,update,order)
             update.message.reply_text(val)
-        
-
-
-        
+            
+    #enters the else part when all the three values are present.         
     else: 
         flag=0
         print(response['input']['text'])
@@ -322,16 +326,12 @@ def message(bot, update):
                 for j in range(len(final)):
                     if i!=j and 'deleted' not in final[i] and 'deleted' not in final[j]:
                        
-                        
                         if final[i]['types']==final[j]['types'] and final[i]['size']==final[j]['size']:
                             vali=final[i]['types']
                             valj=final[i]['size']
                             valk=int(final[i]['number'])+int(final[j]['number'])
-                            
-                            
                             final[i]['deleted']=1
                             final[j]['deleted']=1
-                            
                             final.append({'types':vali,'size':valj,'number':str(valk)})
                             
                         
@@ -341,26 +341,25 @@ def message(bot, update):
                 if 'deleted' not in i:
                     fi.append(i)
             final=fi
-
+            #generates the final reponse.
             i = 0
             final_reply = ''
             reply = 'Your order is '
             while i < len(final):
                 d = final[i].copy()
-                print('d: ', d)
+                #print('d: ', d)
                 if 'types' in d and 'size' in d and 'number' in d:
-                    final_reply += d['number'] + ' ' + d['types'] + ' in ' + d['size'] + '.'
+                    final_reply += d['number'] + ' ' + d['types'] + ' in ' + d['size'] + '. '
                     i = i + 1
             final_reply = reply + final_reply + ' Correct?'
             print(final_reply)
             update.message.reply_text(final_reply)
-        elif 'anything' in context and context['anything']=='yes' and response['input']['text'].lower()!='no':
-            update.message.reply_text('Please place your next order')
+        elif 'anything' in context and context['anything']=='yes' and response['input']['text'].lower()=='yes':
+            update.message.reply_text('Please continue placing your order.')
         elif response['intents'][0]['intent']=='reorder':
              finalord.clear()
              flag=1
              resp2 = send_tele(response)
-             
              update.message.reply_text(resp2)
              
         elif response['intents'][0]['intent']=='get_menu':
@@ -380,50 +379,44 @@ def message(bot, update):
             flag=1
             print("FINAL ORDER")
             print(final)
-            print(len(final))
             
-	    #inserting order
+            
+	    #inserting order into database
             try:
                 conn = sqlite3.connect("coffee_order.db")
                 sum1 =0
                 cost=0
                 cur = conn.cursor()
                 print(conn ,cur)
-                cur.execute("SELect * from user_ord")
+                cur.execute("Select * from user_ord")
                 print (cur.fetchone())
                 for i in final:
                                                      
-                    #cur.execute("INSERT INTO user_ord(type,size,number) VALUES(?,?,?)",(i['types'],i['size'],i['number']))
-                    #conn.commit()
+                    cur.execute("INSERT INTO user_ord(type,size,number) VALUES(?,?,?)",(i['types'],i['size'],i['number']))
+                    conn.commit()
                     cur.execute("SELECT * from coffee_types")
                     rows = cur.fetchall()
 
-                   # for row in rows:
-                        #print(row)
+                   
                     cur.execute("SELECT price from coffee_types where type=?",(i['types'].lower(),))
                     p=cur.fetchone()[0]
-                    #p = cur.fetchone()[0]
-                    #print(p)
-                   
+                    
                     k =i['size'].lower()
                     if k == 'regular':
                         cost =(p-5)* int(i['number'])
                    
                     elif k == 'large':
                        cost = (p+5)* int(i['number'] )
-                       print("in elif")
         
                     else:
                         cost = p * int(i['number'])
-                        print("its in else part")
                     
                     sum1+=cost
                    
-               # print(sum1)
+               
                 '''cur.execute("SELECT * FROM user_ord")
        
                 rows = cur.fetchall()
-
                 for row in rows:
                     print(row)
                 '''
@@ -440,21 +433,20 @@ def message(bot, update):
             resp2 = send_tele(response)
             update.message.reply_text(resp2)  
         elif flag==0:
-            print('working?')
             resp2 = send_tele(response)
             update.message.reply_text(resp2)   
-    # build response
+    
        
     
 
-
+#generates response when it recieves the command /help from telegram.
 def help(bot, update):
     print('Received /help command')
-    update.message.reply_text('Help!')
-    
+    update.message.reply_text('Hi I am a coffee ordering bot. You can get menu,place your order and recieve the total amount to be paid')
+#main function.    
 def main():
     # Create the Updater and pass it your bot's token.
-    updater = Updater('Telegran ID')  # TODO
+    updater = Updater('telegram_api')  # TODO
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
@@ -478,3 +470,5 @@ def main():
 if __name__ == '__main__':
    
     main()
+
+
